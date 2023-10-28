2900d17d0151e24cecb82e48fde0dcc6cc2e72ad
NoName <101563175+HuynhNgoc2709@users.noreply.github.com>
2023-10-28 10:29:10 +0700

    Add files via upload

---
+++ Bash/github_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Token GitHub
+GITHUB_TOKEN="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"
+
+# Tên chủ sở hữu repository
+OWNER="HuynhNgoc2709"
+
+# Tên repository
+REPO="CheckUpdateAppMod"
+
+# Tên file cần đọc
+FILE="logWeb"
+
+# Đường dẫn URL đến file
+URL="https://api.github.com/repos/$OWNER/$REPO/contents/$FILE"
+
+# Lấy nội dung file từ GitHub API
+RESPONSE=$(curl -H "Authorization: token $GITHUB_TOKEN" $URL)
+
+# Lấy giá trị mã hóa base64 của nội dung file từ JSON response
+CONTENT_BASE64=$(echo "$RESPONSE" | jq -r '.content')
+
+# Giải mã base64 nội dung file
+CONTENT=$(echo "$CONTENT_BASE64" | base64 -d)
+
+# Sửa 5 ký tự cuối cùng thành random string 5 ký tự
+NEW_CONTENT=$(echo "$CONTENT" | sed "s/.....$/$(openssl rand -hex 5)/")
+
+# In ra nội dung sau khi sửa đổi
+echo "$NEW_CONTENT"
+
+# Kết thúc script
